497bad91041b154b50d60deb8b93ae7d2101447f
Isabel Drost-Fromm <isabel.drostfromm@elasticsearch.com>
2014-01-07 14:20:53 +0100

    Add test and code for template query builder.

--- src/test/java/org/elasticsearch/script/querytemplate/QueryTemplateFilterTest.java
+++ src/test/java/org/elasticsearch/script/querytemplate/QueryTemplateFilterTest.java
@@ -1,3 +1,22 @@
+/**
+ *
+ * Licensed to ElasticSearch and Shay Banon under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. ElasticSearch licenses this
+ * file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.elasticsearch.script.querytemplate;
 
 import static org.elasticsearch.client.Requests.refreshRequest;
@@ -9,6 +28,10 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.lucene.queryparser.xml.QueryBuilderFactory;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.Client;
@@ -16,6 +39,7 @@ import org.elasticsearch.common.logging.ESLogger;
 import org.elasticsearch.common.logging.Loggers;
 import org.elasticsearch.common.network.NetworkUtils;
 import org.elasticsearch.common.settings.ImmutableSettings;
+import org.elasticsearch.index.query.MatchQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.node.Node;
 import org.elasticsearch.node.NodeBuilder;
@@ -63,7 +87,13 @@ public class QueryTemplateFilterTest {
                 .execute().actionGet();
         client.admin().indices().refresh(refreshRequest()).actionGet();
 
-        SearchResponse sr = client.prepareUpdate().setScript(script);
+        Map<String, Object> vars = new HashMap<String, Object>();
+        vars.put("template", "42");
+        TemplateQueryBuilder builder = new TemplateQueryBuilder("this is my $template query", vars);
+        SearchResponse sr = client.prepareSearch()
+                .setQuery(builder)
+                .execute()
+                .actionGet();
     }
 
 
---
+++ src/test/java/org/elasticsearch/script/querytemplate/TemplateQueryBuilderTest.java
@@ -0,0 +1,44 @@
+/**
+ *
+ * Licensed to ElasticSearch and Shay Banon under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. ElasticSearch licenses this
+ * file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.elasticsearch.script.querytemplate;
+
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.elasticsearch.common.xcontent.XContentFactory;
+import org.junit.Test;
+
+public class TemplateQueryBuilderTest {
+
+    @Test
+    public void testJSONGeneration() throws IOException {
+        Map<String, Object> vars = new HashMap<String, Object>();
+        vars.put("template", "filled");
+        TemplateQueryBuilder builder = new TemplateQueryBuilder("I am a $template string", vars);
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        builder.doXContent(XContentFactory.jsonBuilder(stream), null);
+        assertTrue(stream.toString().equals("{\"template\":{\"template_string\":\"I am a $template string\",\"template_vars\":{\"template\":\"filled\"}}}"));
+    }
+
+}
