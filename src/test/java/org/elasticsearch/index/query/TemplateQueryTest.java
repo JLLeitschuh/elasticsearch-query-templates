2e42185efc93b136b978be6cf1691bb582cf534b
Isabel Drost-Fromm <isabel.drostfromm@elasticsearch.com>
2014-01-08 22:23:19 +0100

    Fix unit test/ remove TODOs

--- src/test/java/org/elasticsearch/index/query/TemplateQueryTest.java
+++ src/test/java/org/elasticsearch/index/query/TemplateQueryTest.java
@@ -18,46 +18,27 @@
  */
 package org.elasticsearch.index.query;
 
-import static org.elasticsearch.common.settings.ImmutableSettings.settingsBuilder;
-
-import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
-
-import java.util.HashMap;
-import java.util.Map;
+import static junit.framework.Assert.assertEquals;
 
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.Client;
-import org.elasticsearch.cluster.ClusterService;
-import org.elasticsearch.common.inject.AbstractModule;
-import org.elasticsearch.common.inject.Injector;
-import org.elasticsearch.common.inject.ModulesBuilder;
-import org.elasticsearch.common.inject.util.Providers;
 import org.elasticsearch.common.logging.ESLogger;
 import org.elasticsearch.common.logging.Loggers;
 import org.elasticsearch.common.network.NetworkUtils;
 import org.elasticsearch.common.settings.ImmutableSettings;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.settings.SettingsModule;
-import org.elasticsearch.index.Index;
-import org.elasticsearch.index.IndexNameModule;
-import org.elasticsearch.index.analysis.AnalysisModule;
-import org.elasticsearch.index.cache.IndexCacheModule;
-import org.elasticsearch.index.codec.CodecModule;
-import org.elasticsearch.index.engine.IndexEngineModule;
-import org.elasticsearch.index.mapper.MapperServiceModule;
-import org.elasticsearch.index.query.IndexQueryParserModule;
 import org.elasticsearch.index.query.TemplateQueryBuilder;
 import org.elasticsearch.index.query.TemplateQueryParser;
-import org.elasticsearch.index.settings.IndexSettingsModule;
-import org.elasticsearch.index.similarity.SimilarityModule;
-import org.elasticsearch.indices.query.IndicesQueriesModule;
-import org.elasticsearch.indices.query.IndicesQueriesRegistry;
 import org.elasticsearch.node.Node;
 import org.elasticsearch.node.NodeBuilder;
-import org.elasticsearch.script.ScriptModule;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
 
 /**
  * Integration test for template based queries: Go from templated search request to search response.
@@ -72,24 +53,27 @@ public class TemplateQueryTest {
 
     @Before
     public void createNodes() throws Exception {
-        node = NodeBuilder.nodeBuilder().settings(ImmutableSettings.settingsBuilder()
+        Settings settings = ImmutableSettings.settingsBuilder()
+                .put("index.queryparser.query.template.type", TemplateQueryParser.class)
+                .put("index.cache.filter.type", "none")
                 .put("cluster.name", "test-cluster-" + NetworkUtils.getLocalAddress())
                 .put("gateway.type", "none")
-                .put("number_of_shards", 1)).node();
+                .put("number_of_shards", 1)
+                .build();
+
+
+        node = NodeBuilder.nodeBuilder().settings(settings).node();
         client = node.client();
     }
 
     @After
     public void closeNodes() {
-        client.close();
-        node.close();
+        if (client != null) client.close();
+        if (node != null) node.close();
     }
 
     @Test
-    //TODO disabled for now - does not work yet
     public void testTemplateQueryExecution() throws Exception {
-        registerTemplateQuery();
-        
         client.admin().indices().prepareCreate("test").execute().actionGet();
         client.prepareIndex("test", "type1", "1")
                 .setSource(jsonBuilder().startObject().field("test", "value beck").field("num1", 1.0f).endObject())
@@ -104,45 +88,13 @@ public class TemplateQueryTest {
                 .execute().actionGet();
         
         Map<String, Object> vars = new HashMap<String, Object>();
-        vars.put("template", "beck");
+        vars.put("template", "all");
         
         TemplateQueryBuilder builder = new TemplateQueryBuilder("{\"match_{{template}}\": {}}\"", vars);
         SearchResponse sr = client.prepareSearch()
                 .setQuery(builder)
                 .execute()
                 .actionGet();
-        // TODO check search response
-    }
-    
-    
-    private void registerTemplateQuery() {
-        Settings settings = ImmutableSettings.settingsBuilder()
-                .put("index.queryparser.query.template.type", TemplateQueryParser.class)
-                .put("index.cache.filter.type", "none")
-                .build();
-        Index index = new Index("test");
-        Injector injector = new ModulesBuilder().add(
-                new CodecModule(settings),
-                new SettingsModule(settings),
-                new IndicesQueriesModule(),
-                new ScriptModule(settings),
-                new MapperServiceModule(),
-                new IndexSettingsModule(index, settings),
-                new IndexCacheModule(settings),
-                new AnalysisModule(settings),
-                new IndexEngineModule(settings),
-                new SimilarityModule(settings),
-                new IndexQueryParserModule(settings),
-                new IndexNameModule(index),
-                new AbstractModule() {
-                    @Override
-                    protected void configure() {
-                        bind(ClusterService.class).toProvider(Providers.of((ClusterService) null));
-                    }
-                }
-        ).createInjector();
-
-        IndicesQueriesRegistry registry = injector.getInstance(IndicesQueriesRegistry.class);
-        registry.addQueryParser(new TemplateQueryParser());
+        assertEquals("Template query didn't return correct number of hits.", 2, sr.getHits().totalHits());
     }
 }
