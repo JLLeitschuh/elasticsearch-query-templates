ffb561aa8dd4ce8db4507c160a99c0250ef6893b
Isabel Drost-Fromm <isabel.drostfromm@elasticsearch.com>
2014-01-08 12:13:28 +0100

    Move integration test of custom query.
    This really should be located next to the remaining template query tests as it
    is an integration test of said template based query.

---
+++ src/test/java/org/elasticsearch/index/query/TemplateQueryTest.java
@@ -0,0 +1,148 @@
+/**
+ * Licensed to Elasticsearch under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.elasticsearch.index.query;
+
+import static org.elasticsearch.common.settings.ImmutableSettings.settingsBuilder;
+
+import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.cluster.ClusterService;
+import org.elasticsearch.common.inject.AbstractModule;
+import org.elasticsearch.common.inject.Injector;
+import org.elasticsearch.common.inject.ModulesBuilder;
+import org.elasticsearch.common.inject.util.Providers;
+import org.elasticsearch.common.logging.ESLogger;
+import org.elasticsearch.common.logging.Loggers;
+import org.elasticsearch.common.network.NetworkUtils;
+import org.elasticsearch.common.settings.ImmutableSettings;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.settings.SettingsModule;
+import org.elasticsearch.index.Index;
+import org.elasticsearch.index.IndexNameModule;
+import org.elasticsearch.index.analysis.AnalysisModule;
+import org.elasticsearch.index.cache.IndexCacheModule;
+import org.elasticsearch.index.codec.CodecModule;
+import org.elasticsearch.index.engine.IndexEngineModule;
+import org.elasticsearch.index.mapper.MapperServiceModule;
+import org.elasticsearch.index.query.IndexQueryParserModule;
+import org.elasticsearch.index.query.TemplateQueryBuilder;
+import org.elasticsearch.index.query.TemplateQueryParser;
+import org.elasticsearch.index.settings.IndexSettingsModule;
+import org.elasticsearch.index.similarity.SimilarityModule;
+import org.elasticsearch.indices.query.IndicesQueriesModule;
+import org.elasticsearch.indices.query.IndicesQueriesRegistry;
+import org.elasticsearch.node.Node;
+import org.elasticsearch.node.NodeBuilder;
+import org.elasticsearch.script.ScriptModule;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Integration test for template based queries: Go from templated search request to search response.
+ * */
+public class TemplateQueryTest {
+
+    protected final ESLogger logger = Loggers.getLogger(getClass());
+
+    private Node node;
+
+    private Client client;
+
+    @Before
+    public void createNodes() throws Exception {
+        node = NodeBuilder.nodeBuilder().settings(ImmutableSettings.settingsBuilder()
+                .put("cluster.name", "test-cluster-" + NetworkUtils.getLocalAddress())
+                .put("gateway.type", "none")
+                .put("number_of_shards", 1)).node();
+        client = node.client();
+    }
+
+    @After
+    public void closeNodes() {
+        client.close();
+        node.close();
+    }
+
+    @Test
+    //TODO disabled for now - does not work yet
+    public void testTemplateQueryExecution() throws Exception {
+        registerTemplateQuery();
+        
+        client.admin().indices().prepareCreate("test").execute().actionGet();
+        client.prepareIndex("test", "type1", "1")
+                .setSource(jsonBuilder().startObject().field("test", "value beck").field("num1", 1.0f).endObject())
+                .execute().actionGet();
+        client.admin().indices().prepareFlush().execute().actionGet();
+        client.prepareIndex("test", "type1", "2")
+                .setSource(jsonBuilder().startObject().field("test", "value beck").field("num1", 2.0f).endObject())
+                .execute().actionGet();
+        client.admin().indices().prepareFlush().execute().actionGet();
+        client.prepareIndex("test", "type1", "3")
+                .setSource(jsonBuilder().startObject().field("test", "value beck").field("num1", 3.0f).endObject())
+                .execute().actionGet();
+        
+        Map<String, Object> vars = new HashMap<String, Object>();
+        vars.put("template", "beck");
+        
+        TemplateQueryBuilder builder = new TemplateQueryBuilder("{\"match_{{template}}\": {}}\"", vars);
+        SearchResponse sr = client.prepareSearch()
+                .setQuery(builder)
+                .execute()
+                .actionGet();
+        // TODO check search response
+    }
+    
+    
+    private void registerTemplateQuery() {
+        Settings settings = ImmutableSettings.settingsBuilder()
+                .put("index.queryparser.query.template.type", TemplateQueryParser.class)
+                .put("index.cache.filter.type", "none")
+                .build();
+        Index index = new Index("test");
+        Injector injector = new ModulesBuilder().add(
+                new CodecModule(settings),
+                new SettingsModule(settings),
+                new IndicesQueriesModule(),
+                new ScriptModule(settings),
+                new MapperServiceModule(),
+                new IndexSettingsModule(index, settings),
+                new IndexCacheModule(settings),
+                new AnalysisModule(settings),
+                new IndexEngineModule(settings),
+                new SimilarityModule(settings),
+                new IndexQueryParserModule(settings),
+                new IndexNameModule(index),
+                new AbstractModule() {
+                    @Override
+                    protected void configure() {
+                        bind(ClusterService.class).toProvider(Providers.of((ClusterService) null));
+                    }
+                }
+        ).createInjector();
+
+        IndicesQueriesRegistry registry = injector.getInstance(IndicesQueriesRegistry.class);
+        registry.addQueryParser(new TemplateQueryParser());
+    }
+}
